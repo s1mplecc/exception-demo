58b6bc51af17ac3737d9ee731d8692e0a2673eeb
s1mple <798342418@qq.com>
2018-07-19 23:34:10 +0800

    #exception-demo - s1mple - 在 Controller 层对异常进行捕获，记录日志并返回给前端错误信息

--- exception-demo-server/src/main/java/info/s1mple/exceptiondemo/rest/controllers/UserController.java
+++ exception-demo-server/src/main/java/info/s1mple/exceptiondemo/rest/controllers/UserController.java
@@ -1,16 +1,23 @@
 package info.s1mple.exceptiondemo.rest.controllers;
 
+import info.s1mple.exceptiondemo.domian.exception.RequestArgsIllegalException;
 import info.s1mple.exceptiondemo.domian.model.user.User;
 import info.s1mple.exceptiondemo.domian.model.user.UserRepository;
 import info.s1mple.exceptiondemo.domian.model.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Resource
     private UserRepository userRepository;
@@ -23,7 +30,13 @@ public class UserController {
     }
 
     @PostMapping
-    public void register(@RequestBody User user) {
-        userService.register(user);
+    public void register(@RequestBody User user, HttpServletResponse response) throws IOException {
+        try {
+            userService.register(user);
+        } catch (RequestArgsIllegalException e) {
+            logger.warn("{}. {}", e.getMessage(), user);
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
     }
 }

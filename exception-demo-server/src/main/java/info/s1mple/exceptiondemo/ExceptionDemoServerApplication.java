bd556f03589e5fa4af9056b5792aff03695490e9
s1mple <798342418@qq.com>
2018-07-19 23:19:52 +0800

    #exception-demo - s1mple - 新增 Module，创建 Spring Boot 项目

---
+++ exception-demo-server/src/main/java/info/s1mple/exceptiondemo/ExceptionDemoServerApplication.java
@@ -0,0 +1,12 @@
+package info.s1mple.exceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionDemoServerApplication.class, args);
+	}
+}

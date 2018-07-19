5d491ddd021af10942e0bdfb434097a87a1f4efd
s1mple <798342418@qq.com>
2018-07-19 23:33:33 +0800

    #exception-demo - s1mple - 在 Service 层的注册方法中添加卫语句，当请求参数违反业务要求时抛出受控异常

--- exception-demo-server/src/main/java/info/s1mple/exceptiondemo/domian/model/user/UserService.java
+++ exception-demo-server/src/main/java/info/s1mple/exceptiondemo/domian/model/user/UserService.java
@@ -1,5 +1,6 @@
 package info.s1mple.exceptiondemo.domian.model.user;
 
+import info.s1mple.exceptiondemo.domian.exception.RequestArgsIllegalException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,7 +11,14 @@ public class UserService {
     @Resource
     private UserRepository userRepository;
 
-    public void register(User user) {
+    public void register(User user) throws RequestArgsIllegalException {
+        if (!user.checkAgeIsLegal()) {
+            throw new RequestArgsIllegalException("age cannot be less than zero");
+        }
+        if (!user.checkUsernameIsNotEmpty()) {
+            throw new RequestArgsIllegalException("username can not be empty");
+        }
+
         userRepository.addUser(new User(user));
     }
 }
